f7fe477d03c36e32a1fd9cf564f0b9b623553781
Petri Airio <petri.j.airio@gmail.com>
2025-09-24 10:18:02 +0300

    fix: remove not used hello import

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import './App.css';
-import Hello from './components/Hello';
 import LoginPage from './components/LoginPage';
 import PrivateRoute from './components/PrivateRoute';
 import {AuthProvider} from './hooks/useAuth';
